3fb438d4c2fb1c95aa0742ffb9abe6b7b698485f
0CrazyLove <axelvasquez582@gmail.com>
2026-08-05 00:40:17 -0500

    solve two sum

---
+++ TwoSum/Program.cs
@@ -0,0 +1,16 @@
+﻿// https://leetcode.com/problems/two-sum/description/
+
+static int[] TwoSum(int[] nums, int target)
+{
+    Dictionary<int, int> numToIndex = [];
+
+    for (int i = 0; i < nums.Length; i++)
+    {
+        if (numToIndex.ContainsKey(target - nums[i]))
+            return [numToIndex[target - nums[i]], i];
+
+        if (!numToIndex.ContainsKey(nums[i]))
+            numToIndex.Add(nums[i], i);
+    }
+    return [];
+}
