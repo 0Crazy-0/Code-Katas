e8f34daa03490d0f660cab9ffcd4c46f204b62a1
0CrazyLove <axelvasquez582@gmail.com>
2026-08-05 00:41:39 -0500

    Solve contains duplicate

---
+++ ContainsDuplicate/Program.cs
@@ -0,0 +1,15 @@
+﻿// https://leetcode.com/problems/contains-duplicate/
+
+static bool ContainsDuplicate(int[] nums)
+{
+    HashSet<int> ints = [];
+
+    for (int x = 0; x < nums.Length; x++)
+    {
+        if (ints.Contains(nums[x]))
+            return true;
+
+        ints.Add(nums[x]);
+    }
+    return false;
+}
