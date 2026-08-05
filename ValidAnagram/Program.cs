ccd7a7c2651462a2b57400bee5c02943ea0e709c
0CrazyLove <axelvasquez582@gmail.com>
2026-08-05 00:42:16 -0500

    Solve valid anagram

---
+++ ValidAnagram/Program.cs
@@ -0,0 +1,22 @@
+﻿// https://leetcode.com/problems/valid-anagram/description/
+
+static bool IsAnagram(string s, string t)
+{
+    if (s.Length != t.Length)
+        return false;
+
+    int[] Counts = new int[26];
+
+    for (var i = 0; i < s.Length; i++)
+    {
+        Counts[s[i] - 'a']++;
+        Counts[t[i] - 'a']--;
+    }
+
+    foreach (var count in Counts)
+    {
+        if (count is not 0)
+            return false;
+    }
+    return true;
+}
