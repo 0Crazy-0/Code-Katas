cb4141f86e10677159019315b33ddcb94ca0a8f0
0CrazyLove <axelvasquez582@gmail.com>
2026-08-05 00:44:06 -0500

    Solve group anagrams

---
+++ GroupAnagrams/Program.cs
@@ -0,0 +1,21 @@
+﻿// https://leetcode.com/problems/group-anagrams/description/
+
+static List<List<string>> GroupAnagrams(string[] strs)
+{
+    Dictionary<string, List<string>> res = [];
+
+    foreach (var s in strs)
+    {
+        var charArray = s.ToCharArray();
+
+        charArray.Sort();
+
+        string sortedS = new(charArray);
+
+        if (!res.ContainsKey(sortedS))
+            res[sortedS] = [];
+
+        res[sortedS].Add(s);
+    }
+    return [.. res.Values];
+}
