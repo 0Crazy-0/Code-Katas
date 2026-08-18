02933b30ef4f360fe95804f5a4be033cb9119274
0CrazyLove <axelvasquez582@gmail.com>
2026-08-17 23:33:16 -0500

    Solve Longest Consecutive Sequence

---
+++ LongestConsecutiveSequence/Program.cs
@@ -0,0 +1,32 @@
+﻿// https://leetcode.com/problems/longest-consecutive-sequence/description/
+
+static int LongestConsecutive(int[] nums)
+{
+    if (nums.Length is 0)
+        return 0;
+
+    HashSet<int> values = [];
+
+    foreach (var n in nums)
+        values.Add(n);
+
+    var maxLength = 0;
+
+    foreach (var num in values)
+    {
+        if (!values.Contains(num - 1))
+        {
+            var currentLength = 1;
+
+            while (values.Contains(num + currentLength))
+            {
+                currentLength++;
+            }
+
+            if (currentLength > maxLength)
+                maxLength = currentLength;
+        }
+    }
+
+    return maxLength;
+}
