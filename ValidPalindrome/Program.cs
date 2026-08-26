b9cf409576b50d7633e577ea2008573a30fbd88e
0CrazyLove <axelvasquez582@gmail.com>
2026-08-26 00:43:31 -0500

    Solve Valid Palindrome

---
+++ ValidPalindrome/Program.cs
@@ -0,0 +1,24 @@
+﻿// https://leetcode.com/problems/valid-palindrome/
+
+static bool IsPalindrome(string s)
+{
+    List<char> clean = [];
+    foreach (var c in s)
+    {
+        if (char.IsLetterOrDigit(c))
+            clean.Add(char.ToLower(c));
+    }
+
+    var left = 0;
+    var right = clean.Count - 1;
+
+    while (left < right)
+    {
+        if (clean[left] != clean[right])
+            return false;
+
+        left++;
+        right--;
+    }
+    return true;
+}
