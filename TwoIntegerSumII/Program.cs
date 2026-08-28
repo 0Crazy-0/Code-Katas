03eaf9a9913070e948e07b5602869870fd5e4f93
0CrazyLove <axelvasquez582@gmail.com>
2026-08-27 23:30:00 -0500

    Solve Two Sum II - Input Array Is Sorted

---
+++ TwoIntegerSumII/Program.cs
@@ -0,0 +1,20 @@
+﻿// https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/description/
+
+static int[] TwoSum(int[] numbers, int target)
+{
+    var left = 0;
+    var right = numbers.Length - 1;
+
+    while (numbers[left] + numbers[right] != target)
+    {
+        if (numbers[left] + numbers[right] < target)
+        {
+            left++;
+        }
+        else
+        {
+            right--;
+        }
+    }
+    return [left + 1, right + 1];
+}
